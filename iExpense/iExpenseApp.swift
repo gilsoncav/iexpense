2d638c8e37336939bd09994ba4c6de295b232402
Gilson Cavalcanti <gilsoncav@gmail.com>
2022-02-25 18:13:02 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Gilson Cavalcanti on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
